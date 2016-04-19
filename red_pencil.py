7b2802aab8ffb0bff2765362ace3dd4e80d1e0d5
Michael Patterson <mjpatter88@gmail.com>
2016-04-19 14:39:12 -0500

    RED: Test that a stable price followed by 25% reduction returns true after the reduction.

--- red_pencil.py
+++ red_pencil.py
@@ -1,2 +1,3 @@
 def is_red_pencil(prices):
+    return [False] * len(prices)
     return False
--- test_red_pencil.py
+++ test_red_pencil.py
@@ -5,4 +5,12 @@ class TestRedPencil(unittest.TestCase):
 
     def test_is_red_pencil__price_not_stable_returns_false(self):
         prices = [100, 100, 100, 85]
-        self.assertEquals(is_red_pencil(prices), False, "Only four days of prices should always be false.")
+        red_pencils = [False] * len(prices)
+        self.assertEquals(is_red_pencil(prices), red_pencils, "Only four days of prices should always be false.")
+
+    def test_is_red_pencil__stable_price_25_discount_returns_true(self):
+        prices = [100] * 30
+        red_pencils = [False] * len(prices)
+        prices.append(75)
+        red_pencils.append(True)
+        self.assertEquals(is_red_pencil(prices), red_pencils, "Stable prices followed by 25% drop should be true.")
