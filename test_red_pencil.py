6961a24031494f32c8fb5211dcaaea834420ada9
Michael Patterson <mjpatter88@gmail.com>
2016-04-19 13:55:31 -0500

    RED: Test that only five days of pricing is never a red pencil special.

--- test_red_pencil.py
+++ test_red_pencil.py
@@ -1,5 +1,8 @@
 import unittest
+from red_pencil import is_red_pencil
 
 class TestRedPencil(unittest.TestCase):
-    def test_first_test(self):
-        self.assertEquals(True, False, "This test should fail.")
+
+    def test_is_red_pencil__price_not_stable_returns_false(self):
+        prices = [100, 100, 100, 85]
+        self.assertEquals(is_red_pencil(prices), False, "Only four days of prices should always be false.")
