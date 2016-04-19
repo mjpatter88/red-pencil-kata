2a4831223c0cc4969f81848e72b27551a23baae0
Michael Patterson <mjpatter88@gmail.com>
2016-04-19 15:10:39 -0500

    GREEN: A stable price followed by 25% reduction returns true after the reduction.

--- red_pencil.py
+++ red_pencil.py
@@ -1,3 +1,19 @@
 def is_red_pencil(prices):
-    return [False] * len(prices)
-    return False
+    is_red_pencils = []
+
+    days_stable = 1
+    old_price = None
+    on_sale = False
+
+    for index, price in enumerate(prices):
+        if price < prices[index-1] * .95 and days_stable >= 30:
+            is_red_pencils.append(True)
+        else:
+            is_red_pencils.append(False)
+        if price == old_price:
+            days_stable += 1
+        else:
+            days_stable = 1
+            old_price = price
+
+    return is_red_pencils
