3354004b2bc68de8aae1f7e6b536580e96315266
Michael Patterson <mjpatter88@gmail.com>
2016-04-21 17:13:38 -0500

    REFACTOR: better names and structure.

--- red_pencil.py
+++ red_pencil.py
@@ -1,6 +1,6 @@
 STABLE_DAYS_LIMIT = 30
-MINIMUM_PRICE_REDUCTION = 5
-MAXIMUM_PRICE_REDUCTION = 30
+MIN_PRICE_REDUCTION = 5
+MAX_PRICE_REDUCTION = 30
 
 def is_red_pencil(prices):
     is_red_pencils = []
@@ -9,11 +9,8 @@ def is_red_pencil(prices):
     old_price = None
     is_on_sale = False
 
-    for index, price in enumerate(prices):
-        if qualifies_for_red_pencil(price, old_price, days_stable) or continue_red_pencil(is_on_sale, price, old_price):
-            is_on_sale = True
-        else:
-            is_on_sale = False
+    for price in prices:
+        is_on_sale = new_red_pencil(price, old_price, days_stable) or continue_red_pencil(is_on_sale, price, old_price)
         is_red_pencils.append(is_on_sale)
 
         if price == old_price:
@@ -27,13 +24,13 @@ def is_red_pencil(prices):
 def continue_red_pencil(is_on_sale, price, old_price):
     return is_on_sale and not price > old_price
     
-def qualifies_for_red_pencil(price, old_price, days_stable):
+def new_red_pencil(price, old_price, days_stable):
     return is_price_reduced(price, old_price) and is_stable(days_stable)
 
 def is_price_reduced(price, old_price):
     if old_price:
-        percent_discount = ((old_price - price) / old_price) * 100
-        return percent_discount >= MINIMUM_PRICE_REDUCTION and percent_discount <= MAXIMUM_PRICE_REDUCTION
+        percent_off = ((old_price - price) / old_price) * 100
+        return percent_off >= MIN_PRICE_REDUCTION and percent_off <= MAX_PRICE_REDUCTION
     else:
         return False
 
