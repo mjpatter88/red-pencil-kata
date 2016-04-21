99d0d13532b5ac01c59326a8125de62105e1b1f6
Michael Patterson <mjpatter88@gmail.com>
2016-04-21 16:12:40 -0500

    GREEN: A red pencil sale continues for more than just one day.

--- red_pencil.py
+++ red_pencil.py
@@ -7,11 +7,12 @@ def is_red_pencil(prices):
 
     for index, price in enumerate(prices):
         price_reduction_percent = ((prices[index-1] - price) / prices[index-1]) * 100
-        print(price_reduction_percent)
-        if price_reduction_percent >= 5 and price_reduction_percent <= 30 and days_stable >= 30:
+        if price_reduction_percent >= 5 and price_reduction_percent <= 30 and days_stable >= 30 or on_sale:
             is_red_pencils.append(True)
+            on_sale = True
         else:
             is_red_pencils.append(False)
+            on_sale = False
         if price == old_price:
             days_stable += 1
         else:
