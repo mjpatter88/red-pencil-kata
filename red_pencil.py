fd728dfedaf1ba3697ac594d8894f535671a821e
Michael Patterson <mjpatter88@gmail.com>
2016-04-21 16:10:50 -0500

    RED: Test that a red pencil sale continues for more than just one day.

--- red_pencil.py
+++ red_pencil.py
@@ -6,7 +6,9 @@ def is_red_pencil(prices):
     on_sale = False
 
     for index, price in enumerate(prices):
-        if price <= prices[index-1] * .95 and days_stable >= 30:
+        price_reduction_percent = ((prices[index-1] - price) / prices[index-1]) * 100
+        print(price_reduction_percent)
+        if price_reduction_percent >= 5 and price_reduction_percent <= 30 and days_stable >= 30:
             is_red_pencils.append(True)
         else:
             is_red_pencils.append(False)
--- test_red_pencil.py
+++ test_red_pencil.py
@@ -21,3 +21,24 @@ class TestRedPencil(unittest.TestCase):
         prices.append(95)
         red_pencils.append(True)
         self.assertEquals(is_red_pencil(prices), red_pencils, "Stable prices followed by 5% drop should be true.")
+
+    def test_is_red_pencil__stable_price_4_discount_returns_false(self):
+        prices = [100] * 30
+        red_pencils = [False] * len(prices)
+        prices.append(96)
+        red_pencils.append(False)
+        self.assertEquals(is_red_pencil(prices), red_pencils, "Stable prices followed by 4% drop should be false.")
+
+    def test_is_red_pencil__stable_price_31_discount_returns_false(self):
+        prices = [100] * 30
+        red_pencils = [False] * len(prices)
+        prices.append(69)
+        red_pencils.append(False)
+        self.assertEquals(is_red_pencil(prices), red_pencils, "Stable prices followed by 31% drop should be false.")
+
+    def test_is_red_pencil__stable_price_25_discount_returns_true_repeatedly_if_sale_continues(self):
+        prices = [100] * 30
+        red_pencils = [False] * len(prices)
+        prices.extend([75] * 5)
+        red_pencils.extend([True] * 5)
+        self.assertEquals(is_red_pencil(prices), red_pencils, "Stable prices followed by 25% drop should be true.")
